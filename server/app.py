52d8a7b147b6e2317688b443df6aa666fd270c0a
dennis-nganga <denniskimani416@gmail.com>
2023-06-20 13:41:57 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask,logging
 
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return f'<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<name>')
+def print_string(name):
+   return f'hello'
+
+@app.route('/print/<text>')
+def print_text(text):
+    app.logger.info(text)
+    return 'Text printed successfully'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
